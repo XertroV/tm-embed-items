fe60f115106cb9f10e64f011e11fac9dbf1a83eb
Max Kaye <m@xk.io>
2023-05-09 20:15:32 +1000

    release trimemd

--- build-release.sh
+++ build-release.sh
@@ -4,8 +4,8 @@ dotnet publish -r linux-x64 -c Release \
      -p:PublishReadyToRun=true \
      -p:PublishSingleFile=true \
      -p:IncludeNativeLibrariesForSelfExtract=true \
+     -p:PublishTrimmed=true \
      --self-contained true
-    #  -p:PublishTrimmed=true \
 
 
 # ./bin/Debug/net6.0/linux-x64/publish/tm-embed-items
